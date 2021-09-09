de3381cfc556e95b34596395d473a32489756e31
istvan kosa <istvankosa@istvans-iMac.home>
2021-09-09 10:32:05 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import { useState } from 'react'
+import Header from './Components/Header'
+import Tasks from './Components/Tasks'
+import AddTask from './Components/AddTask'
+
+function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Sadhana',
+      day: 'Feb 6th at 5:30',
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: 'Blockchain Dev Interview',
+      day: 'Feb 6th at 1:30',
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: 'Hot Date',
+      day: 'Feb 6th at 8:30',
+      reminder: false,
+    },
+  ])
+
+  // add task
+  const addTask = (task) => {
+    console.log(task)
+  }
+
+  // delete task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  // toggle reminder
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => 
+    task.id ===id ? { ...task, reminder: !task.reminder } : task)
+    )
+  }
+
+  return (
+    <div className="container">
+      <Header  />
+      <AddTask onAdd={addTask} />
+      {tasks.length > 0 ? (<Tasks tasks={ tasks } onDelete={ deleteTask } onToggle={toggleReminder} />) : ('No Tasks To Show')}
+    </div>
+  );
+}
+
+export default App;
